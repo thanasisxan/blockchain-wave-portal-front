125a79d1114239526116bfe5515af7612db3dd69
Thanasis Xanthopoulos <thanasis.xanthop@gmail.com>
2022-09-10 23:00:46 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import './App.css';
 export default function App() {
 
   const wave = () => {
-    
+
   }
-  
+
   return (
-    <div className="mainContainer">
+      <div className="mainContainer">
 
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
+        <div className="dataContainer">
+          <div className="header">
+            👋 Hey there!
+          </div>
+
+          <div className="bio">
+            I am thanasisxan and I work as fullstack dev and I am trying to learn more about blockchain development! Connect your Ethereum wallet and wave at me!
+          </div>
+
+          <button className="waveButton" onClick={wave}>
+            Wave at Me
+          </button>
         </div>
-
-        <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
       </div>
-    </div>
   );
 }
