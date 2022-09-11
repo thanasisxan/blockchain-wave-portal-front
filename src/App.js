47bb739fe4119005bafee8ad3ce8025444a1c46f
Thanasis Xanthopoulos <thanasis.xanthop@gmail.com>
2022-09-11 16:08:31 +0300

    connect wallet button

--- src/App.js
+++ src/App.js
@@ -1,62 +1,88 @@
-import { ethers } from "ethers";
-import React, { useEffect, useState } from "react";
+import {ethers} from "ethers";
+import React, {useEffect, useState} from "react";
 import './App.css';
 
 export default function App() {
 
-  /*
-* Just a state variable we use to store our user's public wallet.
-*/
-  const [currentAccount, setCurrentAccount] = useState("");
+    const [currentAccount, setCurrentAccount] = useState("");
 
-  const checkIfWalletIsConnected = async () => {
-    try {
-      const { ethereum } = window;
+    const checkIfWalletIsConnected = async () => {
+        try {
+            const {ethereum} = window;
 
-      if (!ethereum) {
-        console.log("Make sure you have metamask!");
-        return;
-      } else {
-        console.log("We have the ethereum object", ethereum);
-      }
+            if (!ethereum) {
+                console.log("Make sure you have metamask!");
+                return;
+            } else {
+                console.log("We have the ethereum object", ethereum);
+            }
 
-      /*
-      * Check if we're authorized to access the user's wallet
-      */
-      const accounts = await ethereum.request({ method: "eth_accounts" });
+            const accounts = await ethereum.request({method: "eth_accounts"});
 
-      if (accounts.length !== 0) {
-        const account = accounts[0];
-        console.log("Found an authorized account:", account);
-        setCurrentAccount(account)
-      } else {
-        console.log("No authorized account found")
-      }
-    } catch (error) {
-      console.log(error);
+            if (accounts.length !== 0) {
+                const account = accounts[0];
+                console.log("Found an authorized account:", account);
+                setCurrentAccount(account);
+            } else {
+                console.log("No authorized account found")
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
-  }
 
-  useEffect(() => {
-    checkIfWalletIsConnected();
-  }, [])
+    /**
+     * Implement your connectWallet method here
+     */
+    const connectWallet = async () => {
+        try {
+            const {ethereum} = window;
 
-  return (
-      <div className="mainContainer">
+            if (!ethereum) {
+                alert("Get MetaMask!");
+                return;
+            }
 
-        <div className="dataContainer">
-          <div className="header">
-            👋 Hey there!
-          </div>
+            const accounts = await ethereum.request({method: "eth_requestAccounts"});
 
-          <div className="bio">
-            I am thanasisxan and I work as fullstack dev and I am trying to learn more about blockchain development! Connect your Ethereum wallet and wave at me!
-          </div>
+            console.log("Connected", accounts[0]);
+            setCurrentAccount(accounts[0]);
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
-          <button className="waveButton" onClick={null}>
-            Wave at Me
-          </button>
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, [])
+
+
+    return (
+        <div className="mainContainer">
+
+            <div className="dataContainer">
+                <div className="header">
+                    👋 Hey there!
+                </div>
+
+                <div className="bio">
+                    I am thanasisxan and I work as fullstack dev and I am trying to learn more about blockchain
+                    development! Connect your Ethereum wallet and wave at me!
+                </div>
+
+                <button className="waveButton" onClick={null}>
+                    Wave at Me
+                </button>
+
+                {/*
+                * If there is no currentAccount render this button
+                */}
+                {!currentAccount && (
+                    <button className="waveButton" onClick={connectWallet}>
+                        Connect Wallet
+                    </button>
+                )}
+            </div>
         </div>
-      </div>
-  );
+    );
 }
